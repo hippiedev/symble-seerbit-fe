67b1f534dd0ab8afa0eaa7bc086276ec6ec75c67
Jonathan Agusa <nathanagusa@gmail.com>
2022-11-19 20:25:58 +0100

    stuff

--- src/constants/values.ts
+++ src/constants/values.ts
@@ -1,3 +1,3 @@
 export const EXPIRES_IN: number = 3600;
 
-export const BASE_URL: string = 'https://spray-dev.herokuapp.com/api/';
+export const BASE_URL: string = 'https://seerbit-api.onrender.com/';
